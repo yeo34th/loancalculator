d0f0491ec0dca4fbc1a8cfffbb222aaf0f9e82a1
Alex Yeo <changmo.yeo1@gmail.com>
2020-05-18 14:50:56 -0400

    created webapp under django project

---
+++ django_loancalculator/web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpReponse('<h1>Web Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About Home</h1>')
+
+# Create your views here.
