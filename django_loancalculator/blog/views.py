b0a00099ff80c813db4fc3513deb4f55153eba80
Alex Yeo <changmo.yeo1@gmail.com>
2020-05-20 23:05:29 -0400

    After completing Part 5 - Database and Migration

--- django_loancalculator/blog/views.py
+++ django_loancalculator/blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-posts = [
-    {
-	'author': 'Alex Yeo',
-	'title': 'Web Post 1',
-	'content': 'First post content',
-        'date_posted': 'May 19, 2020'
-    },
+from .models import Post
 
-    {
-	'author': 'Changmo Yeo',
-	'title': 'Web Post 2',
-	'content': 'Second post content',
-        'date_posted': 'May 18, 2020'
-    }
-]
 
 def home(request):
     context = {
-	    'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
