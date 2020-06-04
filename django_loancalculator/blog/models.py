dfd0c4109cde9095b70802aa587733f0267bff3e
Alex Yeo <changmo.yeo1@gmail.com>
2020-06-04 05:05:45 -0400

    Part 10 33 min

--- django_loancalculator/blog/models.py
+++ django_loancalculator/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,7 +10,11 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def  __str__(self):
+    def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
 
 # Create your models here.
--- django_loancalculator/blog/views.py
+++ django_loancalculator/blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from django.http import HttpResponse
 from .models import Post
 
@@ -8,6 +13,26 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
